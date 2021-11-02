46dfe02b8db91bcfe644d712338d9b4abad5079a
RajojuSuma <rajojusumalatha9@gmail.com>
2021-11-03 00:05:35 +0530

    first commit

---
+++ src/Tests/HomeTest.java
@@ -0,0 +1,11 @@
+package Tests;
+
+public class HomeTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("print Homepage test");
+
+	}
+
+}
---
+++ src/Tests/Logintest.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class Logintest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("print Loginpage test");
+
+
+	}
+
+}
